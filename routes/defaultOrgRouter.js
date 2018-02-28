116dc3ac256ce2b7a5ceea2e0fc8fbd8d88e8adf
Lance Shi <Lanceyshi@gmail.com>
2018-02-28 13:25:12 +1000

    Add default org router

---
+++ routes/defaultOrgRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cmd = require('node-cmd');
+
+const defaultOrgRouter = express.Router();
+
+defaultOrgRouter.use(bodyParser.json());
+
+defaultOrgRouter.route('/')
+.post((req, res) => {
+    cmd.get(
+        'sfdx force:config:set defaultusername=' + req.body.username + ' --json',
+        function(err, data, stderr) {
+            if(!err) {
+                res.statusCode = 200;
+                res.send(data);
+            } else {
+                res.statusCode = 400;
+                let errObj = JSON.parse(stderr);
+                res.send({"err": errObj.message});
+            }
+        }
+    );
+});
+
+module.exports = defaultOrgRouter;
